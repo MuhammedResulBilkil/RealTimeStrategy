ecbde292c37facca302998d2d6bf79ba990bda0c
Resul <garnnet_221@hotmail.com>
2022-01-30 17:35:41 +0300

    Dealing damage base mechanic added.

---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+public class Health : NetworkBehaviour
+{
+    public event Action OnServerDie;
+    
+    [SerializeField] private int maxHealth = 100;
+
+    [SyncVar] private int _currentHealth;
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        _currentHealth = maxHealth;
+    }
+
+    [Server]
+    public void DealDamage(int damageAmount)
+    {
+        if(_currentHealth == 0) return;
+
+        _currentHealth = Mathf.Max(0, _currentHealth - damageAmount);
+
+        if (_currentHealth != 0) return;
+        
+        OnServerDie?.Invoke();
+        
+        Debug.Log("We Died!");
+    }
+
+    #endregion
+
+    #region Client
+
+    
+
+    #endregion
+}
--- Assets/Scripts/Units/UnitProjectile.cs
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class UnitProjectile : NetworkBehaviour
 {
     [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private int _damageToDeal = 20;
     [SerializeField] private float _destroyAfterSeconds = 5f;
     [SerializeField] private float _launchForce = 10f;
 
@@ -16,6 +17,17 @@ public class UnitProjectile : NetworkBehaviour
     public override void OnStartServer()
     {
         Invoke(nameof(DestroySelf), _destroyAfterSeconds);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out NetworkIdentity networkIdentity))
+            if (networkIdentity.connectionToClient == connectionToClient) return;
+
+        if (other.TryGetComponent(out Health health))
+            health.DealDamage(_damageToDeal);
+                
+        DestroySelf();
     }
 
     [Server]
