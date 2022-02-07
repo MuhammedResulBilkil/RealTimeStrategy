410f2db00c5e556493b9c014a8a4e9efdee47b16
Resul <garnnet_221@hotmail.com>
2022-02-08 02:44:52 +0300

    Building Setup added.
    New Layers added.
    Facing towards to camera logic implemented for UIs.

---
+++ Assets/Scripts/Cameras/FaceCamera.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform _mainCameraTransform;
+
+    private void Awake()
+    {
+        _mainCameraTransform = Camera.main.transform;
+    }
+    
+    private void LateUpdate()
+    {
+        /*Quaternion rotation = Quaternion.LookRotation(_mainCameraTransform.position - transform.position);
+        transform.rotation = rotation;*/
+        
+        /*transform.LookAt(transform.position + _mainCameraTransform.forward,
+            _mainCameraTransform.up);*/
+        
+        // Looking at Camera's forward and up vector
+        transform.LookAt(transform.position + _mainCameraTransform.rotation * Vector3.forward,
+            _mainCameraTransform.rotation * Vector3.up);
+    }
+}
